72490fccf612b21c5f3b1a9ef0f69903b0ac1890
Martinek Vilmos <vilmos.martinek@gmail.com>
2015-05-13 19:15:51 +0200

    PadView commented.

--- padview.h
+++ padview.h
@@ -5,12 +5,25 @@
 #include "pad.h"
 #include "graphics.h"
 
+/**
+ * Az ütõt megjelenítõ osztály.
+ */
 class PadView : public IDrawable {
+    /**
+     * Az ütõ, amit megjelenít.
+     */
     Pad& pad;
 
 public:
+    /**
+     * Konstruktor.
+     * @param pad A megjelenítendõ ütõ.
+     */
     PadView(Pad& pad) : pad(pad) {}
 
+    /**
+     * A kirajzoló függvény.
+     */
     void draw() override final;
 };
 
