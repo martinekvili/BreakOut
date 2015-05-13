b000d1f331da0800970853b7ef731bc99a641ad3
Martinek Vilmos <vilmos.martinek@gmail.com>
2015-05-13 12:33:45 +0200

    Lives shown

--- view.cpp
+++ view.cpp
@@ -21,6 +21,10 @@ void View::draw() {
     std::stringstream ss;
     ss << game->getPoints();
     Graphics::drawText(150, 85, ss.str().c_str());
+
+    for (int i = 0; i < game->getLives(); i++) {
+        Graphics::drawCircle(10 + i * 2.5, 85, 1.0, Graphics::Color{0, 1.0, 0});
+    }
 }
 
 void View::setGame(Game *g) {
