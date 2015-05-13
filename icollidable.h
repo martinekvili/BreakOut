95d7c7983de744f3bcb1ae1d5dd3ef556d34912b
Martinek Vilmos <vilmos.martinek@gmail.com>
2015-05-13 19:11:56 +0200

    ICollidable commented.

--- icollidable.h
+++ icollidable.h
@@ -3,8 +3,33 @@
 
 #include "vector.h"
 
+/**
+ * Az ütközhető osztályok interfésze.
+ */
 struct ICollidable {
+    /**
+     * Az ütközésdetektáló függvény.
+     *
+     * Visszatér a labdától való távolsággal.
+     *
+     * @param start A labda pozíciója.
+     * @param speed A labda sebessége.
+     * @param time A maximális távolság.
+     * @return A legközelebbi távolság (-1, ha nincs ilyen).
+     */
     virtual float tryCollide(Vector start, Vector speed, float time) = 0;
+
+    /**
+     * Az ütközést elvégzõ függvény.
+     *
+     * Beállítja a helyet és a sebességet a megfelelõ értékre,
+     * és levonja az összidõbõl az elhasznált mennyiséget.
+     *
+     * @param start A labda helye.
+     * @param speed A labda sebessége.
+     * @param time A maximális távolság.
+     * @param dist Az elõzõleg kiszámolt távolság.
+     */
     virtual void doCollide(Vector &start, Vector &speed, float &time, float dist) = 0;
 
     virtual ~ICollidable() {}
