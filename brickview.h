aea3d87041d862f1f5256fd65240a4d3be19d8dc
Martinek Vilmos <vilmos.martinek@gmail.com>
2015-05-13 16:42:57 +0200

    BrickView commented

--- brickview.h
+++ brickview.h
@@ -5,14 +5,40 @@
 #include "idrawable.h"
 #include "vector.h"
 
+/**
+ * A téglákat megjelenítõ osztály.
+ */
 class BrickView : public IDrawable {
+    /**
+     * A tégla pozíciója (bal alsó sarok).
+     */
     Vector position;
-    float width, height;
+
+    /**
+     * A tégla szélessége.
+     */
+    float width;
+
+    /**
+     * A tégla magassága.
+     */
+    float height;
 
 public:
+    /**
+     * A konstruktor.
+     * @param position A tégla pozíciója.
+     * @param width A tégla szélessége.
+     * @param height A tégla magassága.
+     */
     BrickView(Vector position, float width, float height) :
         position{position}, width{width}, height{height} {}
 
+    /**
+     * A kirajzoló függvény.
+     *
+     * Egy piros téglalapot rajzol a tégla helyére.
+     */
     void draw() override final;
 };
 
