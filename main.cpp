3753f4df3780d8c99d5c3fbb30e8840277e4c330
Martinek Vilmos <vilmos.martinek@gmail.com>
2015-05-14 18:12:29 +0200

    Main commented

--- main.cpp
+++ main.cpp
@@ -1,21 +1,40 @@
-#include <iostream>
-#include <sstream>
-#include <string>
-
 #include "graphics.h"
 #include "game.h"
 #include "view.h"
 
+/**
+ * A program belépési pontja.
+ *
+ * Ez a függvény szolgál belépési pontként, és controllerként is,
+ * azaz õ végzi el a program inicializálását, majd pedig játék közben
+ * õ kapja meg a GUI-tõl az információkat, amik alapján mozgatja a modellt,
+ * és a kirajzolásnál pedig szintén õ hívja meg a View-t.
+ *
+ * A grafikus megjelenítõnek adandó callback függvények lambda kifejezések.
+ */
 int main(int argc, char **argv) {
+    // A program megjelenítõje
     View view{};
+
+    /*
+     * Maga a játék.
+     *
+     * Azért van indirekció, mivel ezt cserélgetjük: minden újabb kör
+     * újabb játék objektumot jelent.
+     */
     std::shared_ptr<Game> game = std::make_shared<Game>(view);
 
     Graphics::setWorld(160, 90);
 
-    auto paintCallback = [&view] {
-                                view.draw();
-                            };
+    // A kirajzolást kezelõ callback - továbbhív a View-ba.
+    auto paintCallback = [&view] { view.draw(); };
 
+    /*
+     * Az animálást végzõ callback.
+     *
+     * Alapesetben csak simán tovább hív a Game-be,
+     * kivéve ha lebontottunk mindent, akkor új játékot kezd.
+     */
     auto tickCallback = [&game, &view] (float elapsed) {
                             if (game->getGameState() == Game::GameState::won) {
                                 game.reset(new Game{view, game->getPoints(), game->getLives(), game->getRound() + 1});
@@ -24,8 +43,10 @@ int main(int argc, char **argv) {
                             }
                         };
 
+    // Az egérmozgást végzõ callback: csak tovább hív a Game-be.
     auto mouseMotionCallback = [&game] (float x, float y) { game->setPadPosition(x, y); };
 
+    // A balklikket kezelõ callback: ha még nem indult el a játék, akkor elindítja.
     auto leftClickCallback = [&game] {
                                 if (game->getGameState() == Game::GameState::notstarted) {
                                     game->setGameState(Game::GameState::running);
@@ -37,5 +58,11 @@ int main(int argc, char **argv) {
                             leftClickCallback,
                             tickCallback );
 
+    /*
+     * A játékot elindító függvényhívás.
+     *
+     * Akkor és csak akkor tér vissza, amikor a felhasználó
+     * ki x-elni az ablakot.
+     */
     Graphics::initialize(&argc, argv, 960, 540);
 }
